16e0bd287b32d298669ee389b0853a287e32453a
aviraldewan <aviraldewan@gmail.com>
2022-10-25 23:25:41 +0530

    Removed config.js file

--- config.js
+++
@@ -1,6 +0,0 @@
-
-module.exports = {
-    serviceID: "VA7457b25c93df35724f755b6b9f52fb36",
-    accountSID: "AC6e1169d373a22a19aab99d9b580a0168",
-    authToken: "d0c9b05924658e1496d2fca0b81042b6",
-}
