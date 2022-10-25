5ee334e05f1d6cb041fbdf3b3fe8a807c0da66c5
aviraldewan <aviraldewan@gmail.com>
2022-10-26 00:18:42 +0530

    updated config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 module.exports = {
     serviceID: "VA7457b25c93df35724f755b6b9f52fb36",
     accountSID: "AC6e1169d373a22a19aab99d9b580a0168",
-    authToken: "d0c9b05924658e1496d2fca0b81042b6",
+    authToken: "3a831669c780e1b90d06aaad2b40090c",
 }
